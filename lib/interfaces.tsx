b9693e6652a1218ecf44ecf6b97ae895f3ea42b5
Daniel Villegas <avill.daniel@gmail.com>
2022-07-17 20:01:10 -0700

    feat: search bar enables autocompletion when searching country

---
+++ lib/interfaces.tsx
@@ -0,0 +1,28 @@
+export interface LayoutProps {
+  children: React.ReactNode;
+}
+
+export interface SearchProps {
+  countries: Countries[];
+}
+
+export interface Countries {
+  Country: string;
+  Slug: string;
+  ISO2: string;
+}
+
+export interface Country {
+  Country: string;
+  CountryCode: string;
+  Province: string;
+  City: string;
+  CityCode: string;
+  Lat: string;
+  Lon: string;
+  Confirmed: number;
+  Deaths: number;
+  Recovered: number;
+  Active: number;
+  Date: Date;
+}
